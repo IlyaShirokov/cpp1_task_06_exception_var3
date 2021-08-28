4839ca48a501eaa83614138ae4d3f4230421d0da
IlyaShirokov <ilyaacog@gmail.com>
2021-08-29 02:35:09 +0300

    Реализованы, но не протестированы, классы-обертки для контейнера

--- main.cpp
+++ main.cpp
@@ -4,18 +4,67 @@
 
 int main ()
 {
-    OrderedVectorImpl<int> v1;
-    OrderedVectorImpl<int> v2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    OrderedVectorImpl<int> v3 = v2;
-    OrderedVectorImpl<int> v4;
-    v4 = v3;
-    OrderedVectorImpl<int> v5 (std::move(v4));
-    std::cout << "or" << std::endl;
-    int *a = new int[10000];
-    for (int i = 0; i < 10000; ++i)
-    {
-        a[i] = i*i;
-    }
-    delete[] a;
-    std::cout << "or" << std::endl;
+//    std::cout << "--------------Defaul constructor--------------" << std::endl;
+//    OrderedVector<int> v1;
+//    std::cout << v1 << std::endl;
+//    OrderedVector<int> v2(10);
+//    std::cout << v2 << std::endl;
+
+//    std::cout << "--------------Constructor std::initializer_list--------------" << std::endl;
+//    OrderedVector<int> v1 = {12, 45, 123, 887, 1111, 34};
+//    std::cout << v1 << std::endl;
+
+//    std::cout << "--------------Constructor r-value--------------" << std::endl;
+//    OrderedVector<int> v1 = {12, 45, 123, 887, 1111, 34};
+//    std::cout << v1 << std::endl;
+//    OrderedVector<int> v2(std::move(v1));
+//    std::cout << v1 << std::endl;
+//    std::cout << v2 << std::endl;
+
+//    std::cout << "--------------Copy constructor--------------" << std::endl;
+//    OrderedVector<int> v1 = {12, 45, 123, 887, 1111, 34};
+//    std::cout << v1 << std::endl;
+//    OrderedVector<int> v2(v1);
+//    std::cout << v1 << std::endl;
+//    std::cout << v2 << std::endl;
+
+    std::cout << "--------------iterator--------------" << std::endl;
+    OrderedVector<int> v1 = {12, 45, 123, 887, 1111, 34};
+    OrderedVector<const int>::base_iterator it = v1.cbegin();
+    std::cout << *(it+1) << " ";
+    std::cout << *(it+2) << " ";
+    std::cout << *(it+3) << " ";
+
+//    std::cout << "--------------const_iterator--------------" << std::endl;
+//    std::cout << "--------------insert(T&& element)--------------" << std::endl;
+//    std::cout << "--------------insert(const T& element)--------------" << std::endl;
+//    std::cout << "--------------erase(iterator& iter)--------------" << std::endl;
+//    std::cout << "--------------clear()--------------" << std::endl;
+
+//    std::cout << "--------------operator=(OrderedVector<T>&&--------------" << std::endl;
+//    std::cout << "--------------operator=(const OrderedVector<T>&--------------" << std::endl;
+
+//    std::cout << "--------------operator[]--------------" << std::endl;
+
+//    std::cout << "--------------iterator begin()--------------" << std::endl;
+//    std::cout << "--------------const_iterator cbegin()--------------" << std::endl;
+//    std::cout << "--------------iterator end()--------------" << std::endl;
+//    std::cout << "--------------const_iterator cend()--------------" << std::endl;
+
+//    std::cout << "--------------explicit base_iterator(T* pT)--------------" << std::endl;
+//    std::cout << "--------------base_iterator(const base_iterator& rIt)--------------" << std::endl;
+
+//    std::cout << "--------------operator==(const base_iterator& other)--------------" << std::endl;
+//    std::cout << "--------------bool operator!=(const base_iterator& other)--------------" << std::endl;
+
+//    std::cout << "--------------T &operator*();--------------" << std::endl;
+//    std::cout << "--------------const T &operator*() const;--------------" << std::endl;
+//    std::cout << "--------------T* operator->();--------------" << std::endl;
+
+//    std::cout << "--------------base_iterator operator+(int n) const;--------------" << std::endl;
+//    std::cout << "--------------base_iterator operator-(int n) const;--------------" << std::endl;
+//    std::cout << "--------------base_iterator& operator++();--------------" << std::endl;
+//    std::cout << "--------------base_iterator operator++(int);--------------" << std::endl;
+//    std::cout << "--------------base_iterator& operator--();--------------" << std::endl;
+//    std::cout << "--------------base_iterator operator--(int);--------------" << std::endl;
 }
--- ordered_vector.h
+++ ordered_vector.h
@@ -1,5 +1,5 @@
-#ifndef ORDERED_VECTOR_H
-#define ORDERED_VECTOR_H
+#ifndef OrderedVector_H
+#define OrderedVector_H
 
 #include <iostream>
 #include <algorithm>
@@ -26,6 +26,9 @@ void destroy(FwdIter first, FwdIter last)
     }
 }
 
+template < class T >
+class OrderedVector;
+
 template <class T>
 class OrderedVectorImpl
 {
@@ -33,7 +36,7 @@ public:
     OrderedVectorImpl(size_t capacity = 0);
     OrderedVectorImpl(std::initializer_list<T>&& lst);
     OrderedVectorImpl(OrderedVectorImpl<T>&& rrOtherVecImpl);
-    OrderedVectorImpl(OrderedVectorImpl<T>& rOtherVecImpl);
+    OrderedVectorImpl(OrderedVectorImpl<T>& rOtherVecImpl, size_t capacity = 0);
     ~OrderedVectorImpl();
 
     OrderedVectorImpl<T>& operator= (const OrderedVectorImpl<T>& rOtherVecImpl);
@@ -43,10 +46,12 @@ public:
 
     size_t size() const;
     size_t capacity() const;
-    bool is_DataChanged() const;
+    bool is_dataChanged() const;
     void sort();
 
 private:
+    friend class OrderedVector<T>;
+
     T* m_arr;
     size_t m_capacity;
     size_t m_used;
@@ -90,9 +95,9 @@ OrderedVectorImpl<T>::OrderedVectorImpl(OrderedVectorImpl<T>&& rrOtherVecImpl) :
 }
 
 template <class T>
-OrderedVectorImpl<T>::OrderedVectorImpl(OrderedVectorImpl<T>& rOtherVecImpl) :
-    m_arr (static_cast<T*>(rOtherVecImpl.m_capacity == 0 ? nullptr : operator new (sizeof (T) * rOtherVecImpl.m_capacity))),
-    m_capacity (rOtherVecImpl.m_capacity),
+OrderedVectorImpl<T>::OrderedVectorImpl(OrderedVectorImpl<T>& rOtherVecImpl, size_t capacity) :
+    m_arr(static_cast<T*>(std::max(capacity, rOtherVecImpl.capacity()) ? operator new(sizeof(T) * std::max(capacity, rOtherVecImpl.m_capacity)) : nullptr)),
+    m_capacity(std::max(capacity, rOtherVecImpl.m_capacity)),
     m_used (0),
     m_dataChanged (false)
 {
@@ -120,6 +125,9 @@ OrderedVectorImpl<T>& OrderedVectorImpl<T>::operator= (const OrderedVectorImpl<T
     std::cout << "operator= of OrderedVectorImpl" << std::endl;
     if (this != &rOtherVecImpl)
     {
+        destroy(m_arr, m_arr + m_used);
+        operator delete (m_arr);
+
         m_arr = rOtherVecImpl.m_arr;
         m_capacity = rOtherVecImpl.m_capacity;
         m_used = rOtherVecImpl.m_used;
@@ -160,7 +168,7 @@ size_t OrderedVectorImpl<T>::capacity() const
 }
 
 template <class T>
-bool OrderedVectorImpl<T>::is_DataChanged() const
+bool OrderedVectorImpl<T>::is_dataChanged() const
 {
     return m_dataChanged;
 }
@@ -171,4 +179,408 @@ void OrderedVectorImpl<T>::sort()
     std::sort(m_arr, m_arr + m_used);
 }
 
-#endif // ORDERED_VECTOR_H
+template <class T>
+class OrderedVector
+{
+public:
+    class base_iterator;
+    using iterator = typename OrderedVector<T>::base_iterator;
+    using const_iterator = typename OrderedVector<const T>::base_iterator;
+
+    OrderedVector(size_t capacity = 0);
+    OrderedVector(std::initializer_list<T>&& lst);
+    OrderedVector(OrderedVector<T>&& rrOtherVec);
+    OrderedVector(const OrderedVector<T>& rOtherVec);
+    ~OrderedVector();
+
+    iterator find(const T& other);
+    const_iterator find(const T& other) const;
+    void insert(T&& element);
+    void insert(const T& element);
+    void erase(iterator& iter);
+    void clear() noexcept;
+
+    template <class U>
+    friend std::ostream& operator<< (std::ostream &out, const OrderedVector<U> &vec);
+
+    bool is_dataChanged() const;
+    size_t size() const;
+    size_t size_refs() const;
+    size_t capacity() const;
+
+    OrderedVector<T>& operator=(OrderedVector<T>&& rrOtherVec) noexcept;
+    OrderedVector<T>& operator=(const OrderedVector<T>& rcOtherVec) noexcept;
+    T& operator[](size_t i);
+
+    iterator begin();
+    const_iterator cbegin() const;
+    iterator end();
+    const_iterator cend() const;
+
+    class base_iterator
+    {
+    public:
+        explicit base_iterator(T* pT);
+        base_iterator(const base_iterator& rIt);
+
+        bool operator==(const base_iterator& other) const;
+        bool operator!=(const base_iterator& other) const;
+
+        T &operator*();
+        const T &operator*() const;
+        T* operator->();
+
+        base_iterator operator+(int n) const;
+        base_iterator operator-(int n) const;
+        base_iterator& operator++();
+        base_iterator operator++(int);
+        base_iterator& operator--();
+        base_iterator operator--(int);
+
+    private:
+        T* m_Iter;
+
+    };
+
+private:
+    std::shared_ptr<OrderedVectorImpl<T>> m_shrPrtImpl;
+};
+
+template <class T>
+OrderedVector<T>::OrderedVector(size_t capacity) :
+    m_shrPrtImpl(std::make_shared<OrderedVectorImpl<T>>(capacity))
+{
+    std::cout << "Defaul constructor of OrderedVector" << std::endl;
+}
+
+template <class T>
+OrderedVector<T>::OrderedVector(std::initializer_list<T>&& lst) :
+    m_shrPrtImpl(std::make_shared<OrderedVectorImpl<T>>(std::move(lst)))
+{
+    std::cout << "Constructor std::initializer_list of OrderedVector" << std::endl;
+    m_shrPrtImpl->sort();
+}
+
+template <class T>
+OrderedVector<T>::OrderedVector(OrderedVector<T>&& rrOtherVec)
+{
+    std::cout << "Constructor r-value of OrderedVector" << std::endl;
+    std::swap(m_shrPrtImpl, rrOtherVec.m_shrPrtImpl);
+}
+
+template <class T>
+OrderedVector<T>::OrderedVector(const OrderedVector<T>& rOtherVec)
+{
+    std::cout << "Copy constructor of OrderedVector" << std::endl;
+    if (rOtherVec.is_dataChanged())
+        m_shrPrtImpl = std::make_shared<OrderedVectorImpl<T>>(*rOtherVec.m_shrPrtImpl);
+    else
+        m_shrPrtImpl = rOtherVec.m_shrPrtImpl;
+    std::cout << "Refs on m_shrPrtImpl: " << m_shrPrtImpl.use_count() << std::endl;
+}
+
+template <class T>
+OrderedVector<T>::~OrderedVector()
+{
+    std::cout << "Destructor of OrderedVector" << std::endl;
+}
+
+template <class T>
+typename OrderedVector<T>::base_iterator OrderedVector<T>::find(const T& other)
+{
+    for (auto it = begin(); it != end(); ++it)
+    {
+        if (*it == other)
+        {
+            if (m_shrPrtImpl.use_count() > 1)
+            {
+                std::ptrdiff_t diff = m_shrPrtImpl->m_arr - &*it;
+                m_shrPrtImpl.reset(new OrderedVectorImpl<T>(*m_shrPrtImpl));
+//                std::ptrdiff_t diff = &*it - m_shrPrtImpl->m_pArr;
+//                OrderedVectorImpl<T>* temp = new OrderedVectorImpl<T>(*m_shrPrtImpl);
+//                m_shrPrtImpl.reset(temp);
+//                it = typename OrderedVector<T>::iterator(&m_shrPrtImpl->m_arr[diff]);
+            }
+            m_shrPrtImpl->m_dataChanged = true;
+            return it;
+        }
+    }
+    return end();
+}
+
+template <class T>
+typename OrderedVector<T>::const_iterator OrderedVector<T>::find(const T& other) const
+{
+    for (auto it = cbegin(); it != cend(); ++it)
+        if (*it == other)
+            return it;
+    return cend();
+}
+
+template <class T>
+void OrderedVector<T>::insert(T&& element)
+{
+    std::cout << "insert T&&" << std::endl;
+    if (m_shrPrtImpl->capacity() < m_shrPrtImpl->m_used + 1 || m_shrPrtImpl.use_count() > 1)
+    {
+        size_t capacity_new = m_shrPrtImpl->size() + 1 > m_shrPrtImpl->capacity() ? m_shrPrtImpl->size() + 5 : m_shrPrtImpl->capacity();
+        OrderedVectorImpl<T>* temp = new OrderedVectorImpl<T>(*m_shrPrtImpl, capacity_new);
+        construct(temp->m_arr + temp->size(), std::forward<T>(element));
+        temp->m_used ++;
+        m_shrPrtImpl.reset(temp);
+    }
+    else
+    {
+        construct(m_shrPrtImpl->m_arr + m_shrPrtImpl->size(), std::forward<T>(element));
+        m_shrPrtImpl->m_used ++;
+    }
+    m_shrPrtImpl->sort();
+}
+
+template <class T>
+void OrderedVector<T>::insert(const T& element)
+{
+    std::cout << "insert const T&" << std::endl;
+    if (m_shrPrtImpl->capacity() < m_shrPrtImpl->m_Used + 1 || m_shrPrtImpl.use_count() > 1)
+    {
+        size_t capacity_new = m_shrPrtImpl->size() + 1 > m_shrPrtImpl->capacity() ? m_shrPrtImpl->size() + 5 : m_shrPrtImpl->capacity();
+        OrderedVectorImpl<T>* temp = new OrderedVectorImpl<T>(*m_shrPrtImpl, capacity_new);
+        construct(temp->m_pArr + temp->size(), std::forward<T>(element));
+        temp->m_Used ++;
+        m_shrPrtImpl.reset(temp);
+    }
+    else
+    {
+        construct(m_shrPrtImpl->m_pArr + m_shrPrtImpl->size(), element);
+        m_shrPrtImpl->m_Used ++;
+    }
+    m_shrPrtImpl->sort();
+}
+
+template <class T>
+void OrderedVector<T>::erase(iterator& iter)
+{
+    std::ptrdiff_t shift = &*iter - m_shrPrtImpl->m_arr;
+    if (m_shrPrtImpl.use_count() > 1)
+    {
+//        OrderedVectorImpl<T>* temp = new OrderedVectorImpl<T>(*m_shrPrtImpl);
+//        m_shrPrtImpl.reset(temp);
+        m_shrPrtImpl.reset(new OrderedVectorImpl<T>(*m_shrPrtImpl));
+    }
+
+    std::swap(m_shrPrtImpl->m_arr[shift], m_shrPrtImpl->m_arr[m_shrPrtImpl->m_used - 1]);
+    m_shrPrtImpl->m_Used --;
+    m_shrPrtImpl->sort();
+}
+
+template <class T>
+void OrderedVector<T>::clear() noexcept
+{
+    m_shrPrtImpl.reset();
+}
+
+template <class T>
+bool OrderedVector<T>::is_dataChanged() const
+{
+    return m_shrPrtImpl->is_dataChanged();
+}
+
+template <class T>
+size_t OrderedVector<T>::size() const
+{
+    return m_shrPrtImpl->m_used;
+}
+
+template <class T>
+size_t OrderedVector<T>::size_refs() const
+{
+    return m_shrPrtImpl.use_count();
+}
+
+template <class T>
+size_t OrderedVector<T>::capacity() const
+{
+    return m_shrPrtImpl->capacity();
+}
+
+template <class T>
+OrderedVector<T>& OrderedVector<T>::operator=(OrderedVector<T>&& rrOtherVec) noexcept
+{
+    std::cout << "OrderedVector: operator= (move)" << std::endl;
+    if (this != &rrOtherVec)
+    {
+        if (m_shrPrtImpl.use_count() > 1)
+        {
+//            OrderedVectorImpl<T>* temp = new OrderedVectorImpl<T>();
+//            m_shrPrtImpl.reset(temp);
+            m_shrPrtImpl.reset(new OrderedVectorImpl<T>());
+        }
+        m_shrPrtImpl->swap(*rrOtherVec.m_shrPrtImpl);
+    }
+    return *this;
+}
+
+template <class T>
+OrderedVector<T>& OrderedVector<T>::operator=(const OrderedVector<T>& rcOtherVec) noexcept
+{
+    std::cout << "OrderedVector: operator =" << std::endl;
+    if (this != &rcOtherVec)
+    {
+        if (rcOtherVec.m_shrPrtImpl->is_dataChanged())
+            m_shrPrtImpl = std::make_shared<OrderedVectorImpl<T>>(*rcOtherVec.m_shrPrtImpl);
+        else
+            m_shrPrtImpl = rcOtherVec.m_shrPrtImpl;
+    }
+    return *this;
+}
+
+template <class T>
+T& OrderedVector<T>::operator[](size_t i)
+{
+    if (m_shrPrtImpl.use_count() > 1)
+    {
+//        OrderedVectorImpl<T>* temp = new OrderedVectorImpl<T>(*m_shrPrtImpl);
+//        m_shrPrtImpl.reset(temp);
+        m_shrPrtImpl.reset(new OrderedVectorImpl<T>(*m_shrPrtImpl));
+    }
+    m_shrPrtImpl->m_dataChanged = true;
+    return m_shrPrtImpl->m_arr[i];
+}
+
+template <class T>
+typename OrderedVector<T>::iterator OrderedVector<T>::begin()
+{
+    if (m_shrPrtImpl.use_count() > 1)
+    {
+//        OrderedVectorImpl<T>* temp = new OrderedVectorImpl<T>(*m_shrPrtImpl);
+//        m_shrPrtImpl.reset(temp);
+        m_shrPrtImpl.reset(new OrderedVectorImpl<T>(*m_shrPrtImpl));
+    }
+    m_shrPrtImpl->m_dataChanged = true;
+    return OrderedVector<T>::iterator(&m_shrPrtImpl->m_arr[0]);
+}
+
+template <class T>
+typename OrderedVector<T>::const_iterator OrderedVector<T>::cbegin() const
+{
+    return OrderedVector<T>::const_iterator(&m_shrPrtImpl->m_arr[0]);
+}
+
+template <class T>
+typename OrderedVector<T>::iterator OrderedVector<T>::end()
+{
+    if (m_shrPrtImpl.use_count() > 1)
+    {
+//        OrderedVectorImpl<T>* temp = new OrderedVectorImpl<T>(*m_shrPrtImpl);
+//        m_shrPrtImpl.reset(temp);
+        m_shrPrtImpl.reset(new OrderedVectorImpl<T>(*m_shrPrtImpl));
+    }
+    m_shrPrtImpl->m_dataChanged = true;
+    return OrderedVector<T>::iterator(&m_shrPrtImpl->m_arr[0] + m_shrPrtImpl->m_used);
+}
+
+template <class T>
+typename OrderedVector<T>::const_iterator OrderedVector<T>::cend() const
+{
+    return OrderedVector<T>::const_iterator(&m_shrPrtImpl->m_arr[0] + m_shrPrtImpl->m_used);
+}
+
+template <class T>
+OrderedVector<T>::base_iterator::base_iterator(T* pT) :
+    m_Iter(pT)
+{}
+template <class T>
+OrderedVector<T>::base_iterator::base_iterator(const base_iterator& rIt) :
+    m_Iter(rIt.m_Iter)
+{}
+
+template <class T>
+bool OrderedVector<T>::iterator::operator==(const base_iterator& other) const
+{
+    return m_Iter == other.m_Iter;
+}
+
+template <class T>
+bool OrderedVector<T>::iterator::operator!=(const base_iterator& other) const
+{
+    return m_Iter != other.m_Iter;
+}
+
+template <class T>
+T& OrderedVector<T>::iterator::operator*()
+{
+    return *m_Iter;
+}
+
+template <class T>
+const T& OrderedVector<T>::iterator::operator*() const
+{
+    return *m_Iter;
+}
+
+template <class T>
+T* OrderedVector<T>::iterator::operator->()
+{
+    return m_Iter;
+}
+
+template <class T>
+typename OrderedVector<T>::base_iterator OrderedVector<T>::iterator::operator+(int n) const
+{
+    return base_iterator(m_Iter + n);
+}
+
+template <class T>
+typename OrderedVector<T>::base_iterator OrderedVector<T>::iterator::operator-(int n) const
+{
+    return base_iterator(m_Iter - n);
+}
+
+template <class T>
+typename OrderedVector<T>::base_iterator& OrderedVector<T>::iterator::operator++()
+{
+    this->m_Iter ++;
+    return *this;
+}
+
+template <class T>
+typename OrderedVector<T>::base_iterator OrderedVector<T>::iterator::operator++(int)
+{
+    base_iterator tmp(m_Iter ++);
+    return tmp;
+}
+
+template <class T>
+typename OrderedVector<T>::base_iterator& OrderedVector<T>::iterator::operator--()
+{
+    this->m_Iter --;
+    return *this;
+}
+
+template <class T>
+typename OrderedVector<T>::base_iterator OrderedVector<T>::iterator::operator--(int)
+{
+    base_iterator tmp(m_Iter --);
+    return tmp;
+}
+
+template <class U>
+std::ostream& operator<< (std::ostream &out, const OrderedVector<U> &vec)
+{
+    if (vec.m_shrPrtImpl != nullptr)
+    {
+        for (auto it = vec.cbegin(); it != vec.cend(); ++it)
+        {
+            out << *it << "   ";
+        }
+
+        out << "size: " << vec.size() << "   ";
+        out << "refs: " << vec.size_refs() << "   ";
+        out << "capacity: " << vec.capacity() << "   ";
+        out << "was changed: : " << (vec.is_dataChanged() ? "Yes" : "No");
+        return out;
+    }
+    out << "Empty";
+    return out;
+}
+#endif // OrderedVector_H
